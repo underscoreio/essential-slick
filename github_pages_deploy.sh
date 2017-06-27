5eea492781153b8c758890bede27e744cc1cba8e
Richard Dallaway <richard@dallaway.com>
2017-06-27 08:50:21 +0100

    checkout books to temp filesystem

--- github_pages_deploy.sh
+++ github_pages_deploy.sh
@@ -13,15 +13,17 @@ chmod 600 ~/.ssh/deploy.key
 git config --global user.email "richard@dallaway.com"
 git config --global user.name "Richard Dallaway"
 
-mkdir tmp
-cd tmp
+mkdir /tmp/dest
+cd /tmp/dest
 git clone git@github.com:underscoreio/books.git
 
-cp ../dist/*.pdf books/essential-slick/
-cp ../dist/*.html books/essential-slick/
-cp ../dist/*.epub books/essential-slick/
+cp ~/dist/*.pdf books/essential-slick/
+cp ~/dist/*.html books/essential-slick/
+cp ~/dist/*.epub books/essential-slick/
 
 git add books/essential-slick
 git commit -m "auto commit Essential Slick via travis $TRAVIS_JOB_NUMBER $TRAVIS_COMMIT [ci skip]"
-  git push git@github.com:underscoreio/books.git master:master
+git push git@github.com:underscoreio/books.git master:master
+
+rm -rf /tmp/dest
 #fi
