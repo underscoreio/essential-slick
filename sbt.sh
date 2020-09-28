296e552f11690c7a8ac6e0920024f96a552de64d
Richard Dallaway <richard@dallaway.com>
2020-09-28 17:55:27 +0100

    fetch recent sbt

--- sbt.sh
+++ sbt.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+# For use in Docker 
+
+# fetch a recent SBT to use:
+mkdir -p ~/bin
+curl -Ls https://git.io/sbt > ~/bin/sbt 
+chmod 0755 ~/bin/sbt
 
 export JAVA_OPTS="-Xmx3g -XX:+TieredCompilation -XX:ReservedCodeCacheSize=256m -XX:+UseNUMA -XX:+UseParallelGC -XX:+CMSClassUnloadingEnabled"
 
-sbt "$@"
+~/bin/sbt "$@"
